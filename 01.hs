21498468f2506abceb805a59432efed4948309ee
jrvieira <github@jrvieira.com>
2020-12-01 21:54:00 +0000

    clean up 01

--- 01.hs
+++ 01.hs
@@ -6,22 +6,22 @@ main = do
    print $ solve 2020 list
    print $ solve' 2020 list
 
-solve :: Int -> [Int] -> Either String Int
-solve _ [] = Left "empty set"
+solve :: Int -> [Int] -> Maybe Int
+solve _ [] = Nothing
 solve t ls = go ls
    where
    set = S.fromList ls
-   go [] = Left "no match"
+   go [] = Nothing
    go (x:xs)
-      | S.member (t - x) set = Right $ x * (t - x)
+      | S.member (t - x) set = Just (x * t - x)
       | otherwise = go xs
 
 -- part 2
 
-solve' :: Int -> [Int] -> Either String Int
-solve' _ [] = Left "empty set"
+solve' :: Int -> [Int] -> Maybe Int
+solve' _ [] = Nothing
 solve' t ls = go ls
    where
    go (x:xs)
-      | Right n <- solve (t - x) ls = Right (x * n)
+      | Just n <- solve (t - x) ls = Just (x * n)
       | otherwise = go xs
