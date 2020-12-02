2b76ae52333f1e9adc66678609ac97055a44717a
jrvieira <github@jrvieira.com>
2020-12-02 22:07:20 +0000

    day 02

--- 02.hs
+++ 02.hs
@@ -1,2 +1,31 @@
+import Data.List.Split
+import Control.Arrow
+import Data.Bool.Unicode
+
 main :: IO ()
-main = print "ok"
+main = do
+   list <- lines <$> readFile "02.txt"
+   print . length . filter id . map validate $ list
+   print . length . filter id . map validate' $ list
+
+data Input = Input Int Int Char String
+
+parse :: String -> Input
+parse s = Input (read i1) (read i2) (head c) pw
+   where
+   [i1,rest] = splitOn "-" s
+   [i2,c,pw] = splitOn " " rest
+
+validate :: String -> Bool
+validate s = uncurry (∧) . ((>= mn) &&& (<= mx)) . length . filter (== ch) $ pw
+   where
+   Input mn mx ch pw = parse s
+
+validate' :: String -> Bool
+validate' s = check i1 ⊻ check i2
+   where
+   check i
+      | i > length pw = False
+      | otherwise = pw !! (i - 1) == ch
+   Input i1 i2 ch pw = parse s
+
