51281149c0842040db710b87ad944d8cabe2f098
jrvieira <github@jrvieira.com>
2020-12-03 06:12:04 +0000

    clean up

--- 03.hs
+++ 03.hs
@@ -6,11 +6,8 @@ main = do
    print . product $ map (($ input) . slope) [(1,1),(3,1),(5,1),(7,1),(1,2)]
 
 slope :: (Int,Int) -> String -> Int
-slope (r,d) = count '#' . tk 0 . every d . lines
+slope (r,d) = count '#' . takes . skips . lines
    where
-   tk _ [] = []
-   tk n (x:xs) = cycle x !! n : tk (n+r) xs
-
-every :: Int -> [a] -> [a]
-every _ [] = []
-every n (x:xs) = x : every n (drop (n-1) xs)
+   skips [] = []
+   skips xs = head xs : skips (drop d xs)
+   takes xs = head <$> zipWith drop [0,r..] (cycle <$> xs)
