749db3cadcf4b4ac749dcae4893341e3f1020b9f
jrvieira <github@jrvieira.com>
2020-12-03 06:33:00 +0000

    clean up

--- 03.hs
+++ 03.hs
@@ -3,6 +3,7 @@ import Zero.Zero
 main :: IO ()
 main = do
    input <- readFile "03.txt"
+   print $ slope (3,1) input
    print . product $ map (($ input) . slope) [(1,1),(3,1),(5,1),(7,1),(1,2)]
 
 slope :: (Int,Int) -> String -> Int
