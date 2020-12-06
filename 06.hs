efbadeedbbc40accc8caea35557078707fae3cb6
jrvieira <github@jrvieira.com>
2020-12-06 07:20:30 +0000

    clean up

--- 06.hs
+++ 06.hs
@@ -1,11 +1,9 @@
-import Zero.Zero
 import Data.List
 import Data.List.Split
-import Control.Monad
 
 main :: IO ()
 main = do
    input <- map (splitOn "\n") . splitOn "\n\n" . init <$> readFile "06.txt"
-   print $ sum $ length . unique . join <$> input
-   print $ sum $ length . foldr1 intersect . unique <$> input
+   print $ sum $ length . foldr1 union <$> input
+   print $ sum $ length . foldr1 intersect <$> input
 
