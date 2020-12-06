465078ea39a5483b39becb994a5623aef17f4482
jrvieira <github@jrvieira.com>
2020-12-06 07:29:26 +0000

    clean up

--- 06.hs
+++ 06.hs
@@ -3,7 +3,7 @@ import Data.List.Split
 
 main :: IO ()
 main = do
-   input <- map (splitOn "\n") . splitOn "\n\n" . init <$> readFile "06.txt"
+   input <- map lines . splitOn "\n\n" . init <$> readFile "06.txt"
    print $ sum $ length . foldr1 union <$> input
    print $ sum $ length . foldr1 intersect <$> input
 
