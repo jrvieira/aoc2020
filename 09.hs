61905dc82dfe8d33725fa34d7f14db2fb4466399
jrvieira <github@jrvieira.com>
2020-12-10 09:46:56 +0000

    clean up

--- 09.hs
+++ 09.hs
@@ -1,11 +1,14 @@
 import Zero.Zero
+import Data.Foldable
+import Data.List
 
 main :: IO ()
 main = do
    input <- map read . lines <$> readFile "09.txt"
 -- test1 <- map read . lines <$> readFile "09.test"
 -- teqt "part 1" 127 $ solve 5 test1
-   print $ solve 25 input
+   print $ solve  25 input
+   print $ solve' (solve 25 input) input
 
 solve :: Int -> [Int] -> Int
 solve p l = go (queue $ take p l) (drop p l)
@@ -18,9 +21,22 @@ solve p l = go (queue $ take p l) (drop p l)
 
 -- recycled from day 01
 fsum :: Int -> Queue Int -> Bool
-fsum t p = go $ foldr (:) [] p
+fsum t q = go $ toList q
    where
    go [] = False
    go (x:xs)
-      | t > x && t /= 2 * x && (t - x) ∈ p = True
+      | t > x , t /= 2 * x , (t - x) ∈ q = True
       | otherwise = go xs
+
+-- part 2
+
+solve' :: Int -> [Int] -> Int
+solve' t l = go [] $ reverse $ tails l
+   where
+   go :: [Int] -> [[Int]] -> Int
+   go _ [] = error "invulnerable"
+   go _ ([]:ls) = go [] ls
+   go pre ((x:xs):ls)
+      | length pre > 1 , sum pre == t = minimum pre + maximum pre
+      | otherwise = go (x : pre) (xs:ls)
+
