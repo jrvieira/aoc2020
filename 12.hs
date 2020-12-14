b396f23b2a96a7f9aa10d08c8a23e9a8be42a910
jrvieira <github@jrvieira.com>
2020-12-14 00:02:03 +0000

    day 12

---
+++ 12.hs
@@ -0,0 +1,37 @@
+import Zero.Zero
+import Control.Arrow
+
+main :: IO ()
+main = do
+   input <- map (head &&& read . tail) . lines <$> readFile "12.txt"
+   test <- map (head &&& read . tail) . lines <$> readFile "12.test"
+   teqt "part 1" 25 $ solve test
+   print $ solve  input
+
+data Position = Π { x :: Int , y :: Int }
+   deriving Show
+
+data Ship = Ship { δ :: Char , π :: Position }
+   deriving Show
+
+solve :: [(Char,Int)] -> Int
+solve = uncurry (+) . (abs . x &&& abs . y) . π . run (Ship 'E' (Π 0 0))
+
+run :: Ship -> [(Char,Int)] -> Ship
+run s [] = s
+run s@(Ship d p) ((di,n):xs)
+-- | False  # show ((di,n),s) = undefined
+   | 'N' <- di = run s { π = Π (x p) (y p + n) } xs
+   | 'S' <- di = run s { π = Π (x p) (y p - n) } xs
+   | 'E' <- di = run s { π = Π (x p + n) (y p) } xs
+   | 'W' <- di = run s { π = Π (x p - n) (y p) } xs
+   | 'F' <- di = run s $ (d,n) : xs
+   | otherwise = run s { δ = t (div n 90) } xs
+   where
+   t x = head . drop x . dropWhile (/= d) $ cycle wise
+      where
+      wise
+         | 'L' <- di = "NWSE"
+         | 'R' <- di = "WNES"
+         | otherwise = error "no parse"
+
