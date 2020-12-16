0a5694c6cacf8afd2e4559167d5f74406ee9386f
jrvieira <github@jrvieira.com>
2020-12-16 11:32:37 +0000

    clean up

--- 15.hs
+++ 15.hs
@@ -1,4 +1,4 @@
-import Zero.Zero
+-- import Zero.Zero
 import Data.List.Split
 import qualified Data.IntMap.Strict as M
 import Control.Arrow
@@ -6,8 +6,8 @@ import Control.Arrow
 main :: IO ()
 main = do
    input <- map read . splitOn "," <$> readFile "15.txt"
-   tests <- map (map read . splitOn ",") . lines <$> readFile "15.test"
-   teqt "part 1" [436,1,10,27,78,438,1836] $ solve 2020 <$> tests
+-- tests <- map (map read . splitOn ",") . lines <$> readFile "15.test"
+-- teqt "part 1" [436,1,10,27,78,438,1836] $ solve 2020 <$> tests
    print $ solve 2020 input
    print $ solve 30000000 input
 
